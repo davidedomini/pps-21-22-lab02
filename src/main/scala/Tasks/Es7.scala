e2f7ac677256ada682ddcfb88c7279bb32206ded
dom <davidedomini@gmail.com>
2022-03-07 22:32:48 +0100

    added module Shape in es 7

--- src/main/scala/Tasks/Es7.scala
+++ src/main/scala/Tasks/Es7.scala
@@ -7,13 +7,18 @@ object Es7 extends App:
     case Circle(radius: Int)
     case Square(side: Int)
 
+  object Shape:
+    def perimeter(shape: Shape): Double = shape match
+      case Shape.Rectangle(b: Int, h: Int) => 2*b + 2*h
+      case Shape.Circle(r: Int) => 2 * 3.14 * r
+      case Shape.Square(s: Int) => 4 * s
 
-  def perimeter(shape: Shape): Double = shape match
-    case Shape.Rectangle(b: Int, h: Int) => 2*b + 2*h
-    case Shape.Circle(r: Int) => 2 * 3.14 * r
-    case Shape.Square(s: Int) => 4 * s
+    def area(shape: Shape): Double = shape match
+      case Shape.Rectangle(b: Int, h: Int) => b * h
+      case Shape.Circle(r: Int) => 3.14 * r * r
+      case Shape.Square(s: Int) => s * s
 
-  def area(shape: Shape): Double = shape match
-    case Shape.Rectangle(b: Int, h: Int) => b * h
-    case Shape.Circle(r: Int) => 3.14 * r * r
-    case Shape.Square(s: Int) => s * s
+
+  import Shape.*
+  println(perimeter(Shape.Rectangle(4,5)))
+  println(area(Shape.Circle(5)))
